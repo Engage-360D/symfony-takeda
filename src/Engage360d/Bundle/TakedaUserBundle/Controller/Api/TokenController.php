93462e639d3aa6ca218024935ad63ea5c5cc9e7a
AliochaRyzhkov <alioch@yandex.ru>
2015-01-15 19:14:41 +0300

    implement /tokens/vk resource

--- src/Engage360d/Bundle/TakedaUserBundle/Controller/Api/TokenController.php
+++ src/Engage360d/Bundle/TakedaUserBundle/Controller/Api/TokenController.php
@@ -15,6 +15,7 @@ class TokenController extends JsonApiController
     const URI_TOKEN_ONE = '/api/v1/schemas/tokens/one.json';
     const URI_TOKEN_POST = '/api/v1/schemas/tokens/post.json';
     const URI_TOKEN_POST_FACEBOOK = '/api/v1/schemas/tokens/facebook/post.json';
+    const URI_TOKEN_POST_VK = '/api/v1/schemas/tokens/vk/post.json';
 
     protected function getTokenResource($token, $user)
     {
@@ -164,6 +165,69 @@ class TokenController extends JsonApiController
         $user = $this->get('doctrine')
             ->getRepository('Engage360dTakedaUserBundle:User\User')
             ->findOneBy(array('facebookId' => $facebookId));
+
+        if (!$user) {
+            return $this->getErrorResponse("The user is not registered", 400);
+        }
+
+        $token = new Token($this->get('lexik_jwt_authentication.jwt_manager')->create($user));
+        $token->setUser($user);
+
+        return new JsonResponse($this->getTokenResource($token, $user), 201);
+    }
+
+    /**
+     * Sign in through vk.com
+     * @Route("/tokens/vk", name="api_post_token_vk", methods="POST")
+     */
+    public function tokensVkAction(Request $request)
+    {
+        if (!$this->isContentTypeValid($request)) {
+            return $this->getErrorResponse("The expected content type is \"application/vnd.api+json\"", 400);
+        }
+
+        // Handle json request
+        $body = $request->getContent();
+        $data = json_decode($body);
+
+        $retriever = new UriRetriever();
+        $schema = $retriever->retrieve(
+            'file://' . $this->get('kernel')->getRootDir() . '/../web' .
+            self::URI_TOKEN_POST_VK
+        );
+
+        $validator = new Validator();
+        $validator->check($data, $schema);
+
+        if (!$validator->isValid()) {
+            return $this->getErrorResponse($validator->getErrors(), 400);
+        }
+
+        $access_token = $data->data->access_token;
+        $vkontakteId = $data->data->user_id;
+
+        // verify that token is valid
+        $buzz = $this->container->get('buzz');
+        $response = $buzz->get(sprintf(
+            'https://api.vk.com/method/users.isAppUser?v=5.27&user_id=%s&access_token=%s',
+            $vkontakteId,
+            urlencode($access_token)
+        ));
+
+        if (!$response->isSuccessful()) {
+            return $this->getErrorResponse("Bad credentials", 400);
+        }
+
+        $body = $response->getContent();
+        $data = json_decode($body);
+
+        if (!isset($data->response) || $data->response !== 1) {
+            return $this->getErrorResponse("Bad credentials", 400);
+        }
+
+        $user = $this->get('doctrine')
+            ->getRepository('Engage360dTakedaUserBundle:User\User')
+            ->findOneBy(array('vkontakteId' => $vkontakteId));
 
         if (!$user) {
             return $this->getErrorResponse("The user is not registered", 400);
