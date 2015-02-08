51a654c4e805e6ae13caf25275a740668d4f02db
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-02-08 14:46:29 +0300

    TestResult + /account/recommendations

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -8,5 +8,6 @@ module.exports = {
   SignInAfterSocial: require('./components/modules/signin/SignInAfterSocial'),
   RiskAnalysis: require('./components/modules/riskAnalysis/RiskAnalysis'),
   OpinionList: require('./components/modules/pressCenter/OpinionList'),
-  NewsList: require('./components/modules/pressCenter/NewsList')
+  NewsList: require('./components/modules/pressCenter/NewsList'),
+  TestResultList: require('./components/modules/riskAnalysis/TestResultList')
 };
