81781e7ac429625b416ba5ed8d63cda2bdac9aef
AliochaRyzhkov <alioch@yandex.ru>
2015-02-16 16:47:44 +0300

    remove TimelineController

--- src/Engage360d/Bundle/TakedaBundle/Controller/AccountController.php
+++ src/Engage360d/Bundle/TakedaBundle/Controller/AccountController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\UserBundle\Model\UserInterface;
+use JMS\Serializer\SerializationContext;
 
 class AccountController extends Controller
 {
@@ -36,6 +37,35 @@ class AccountController extends Controller
             'user' => $this->getUserArray($this->getUser()),
             'regions' => $this->getDoctrine()->getRepository('Engage360dTakedaBundle:Region\Region')->findAll()
         ]);
+    }
+
+    public function timelineAction()
+    {
+        $user = $this->getUser();
+
+        if ($user->getTestResults()->isEmpty()) {
+            return $this->redirect($this->generateUrl('engage360d_takeda_risk_analysis'));
+        }
+
+        $timelineManager = $this->get('engage360d_takeda.timeline_manager');
+        $timelineManager->setUser($user);
+
+        $serializer = $this->get('jms_serializer');
+
+        $timeline = $timelineManager->getTimeline();
+        $context = new SerializationContext();
+        $context->setSerializeNull(true);
+        $timeline = $serializer->serialize($timeline, 'json', $context);
+
+        $pills = array_map([$this, 'getPillArray'], $user->getPills()->toArray());
+        $context = new SerializationContext();
+        $context->setSerializeNull(true);
+        $pills = $serializer->serialize($pills, 'json', $context);
+
+        return $this->render('Engage360dTakedaBundle:Account:timeline.html.twig', array(
+            'timeline' => $timeline,
+            'pills' => $pills,
+        ));
     }
 
     public function logoutAction(Request $request)
@@ -98,6 +128,23 @@ class AccountController extends Controller
             "bmi" => $testResult->getBmi(),
             "score" => $testResult->getScore(),
             "recommendations" => $recommendations,
+        ];
+    }
+
+    // TODO refactor
+    protected function getPillArray($pill)
+    {
+        return [
+            "id" => (string) $pill->getId(),
+            "name" => $pill->getName(),
+            "quantity" => $pill->getQuantity(),
+            "repeat" => $pill->getRepeat(),
+            "time" => $pill->getTime()->format('H:i:s'),
+            "sinceDate" => $pill->getSinceDate()->format(\DateTime::ISO8601),
+            "tillDate" => $pill->getTillDate()->format(\DateTime::ISO8601),
+            "links" => [
+                "user" => (string) $pill->getUser()->getId()
+            ]
         ];
     }
 
