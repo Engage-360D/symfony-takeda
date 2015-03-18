67a850c208d2aaa4cca11a0c6816760201ec59da
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-03-18 12:55:03 +0700

    Good To Know backend

---
+++ src/Engage360d/Bundle/TakedaBundle/Block/GoodToKnow.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Engage360d\Bundle\TakedaBundle\Block;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+
+class GoodToKnow extends BaseBlockService
+{
+    private static $blockId = 0;
+
+    public function setDefaultSettings(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'template' => 'Engage360dTakedaBundle:Block:good_to_know.html.twig',
+            'color' => '',
+            'image' => '',
+            'title' => '',
+            'content' => '',
+        ));
+    }
+
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        $blockSettings = $blockContext->getSettings();
+
+        return $this->renderResponse($blockContext->getTemplate(), array(
+            'color' => $blockSettings['color'],
+            'image' => $blockSettings['image'],
+            'title' => $blockSettings['title'],
+            'content' => $blockSettings['content'],
+            'blockId' => self::$blockId++
+        ), $response);
+    }
+}
